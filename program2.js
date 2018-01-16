57668915d6d2ee5b3d2a4ae967953b0a8537d555
kshiprashetty <kshiprashetty@gmail.com>
2018-01-16 12:39:23 +0530

    check for base case

---
+++ program2.js
@@ -0,0 +1,8 @@
+function doubleAll(numArray)
+{
+        return numArray.map(function(arrayNum){
+                                                return arrayNum*2;
+                                                });
+
+}
+console.log("Should work for base case ", doubleAll([1,2,3,4]).toString()===[2,4,6,8].toString());
