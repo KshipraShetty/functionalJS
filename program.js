e15accbab3395ab150f6dbbd0982dd220fe5b2cd
kshiprashetty <kshiprashetty@gmail.com>
2018-01-16 10:13:24 +0530

    check for number input

--- program.js
+++ program.js
@@ -1,6 +1,6 @@
 
 function upperCase(input) {
-	if(input!='')
+	if(input!='' && typeof(input)==='string')
 		return input.toUpperCase();
 	else
 		return false;	
@@ -10,4 +10,5 @@ function upperCase(input) {
 
 module.exports = upperCase;
 console.log("This should return upper case: ",upperCase("Hello World")==="HELLO WORLD");
-console.log("Check for empty string", upperCase('')===false);
+console.log("Check for empty string:", upperCase('')===false);
+console.log("Check for numbers:", upperCase(12345)===false); 
