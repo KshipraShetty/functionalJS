c32f7cd67308fec1d134b20e5daa9526e558a572
kshiprashetty <kshiprashetty@gmail.com>
2018-01-16 10:01:11 +0530

    True test case with normal string input

---
+++ program.js
@@ -0,0 +1,9 @@
+
+function upperCase(input) {
+	return input.toUpperCase();	
+
+}
+
+
+module.exports = upperCase;
+console.log("This should return upper case: ",upperCase("Hello World")==="HELLO WORLD");
