ff2f10c04b2a27d8906b3c6daf5d44a39ab1b165
kshiprashetty <kshiprashetty@gmail.com>
2018-01-16 12:17:19 +0530

    check for number 0

--- program1.js
+++ program1.js
@@ -2,14 +2,12 @@ let counter=0;
 
 function operation()
 {
-	counter++;
-
-	
+	counter++;	
 }
 
 function repeat(operation, num) {
-	if(num===counter)
-		return counter;
+	if(num===counter){
+		return counter;}
 	else if(num>0)
 		{
 		operation();
@@ -20,4 +18,7 @@ function repeat(operation, num) {
 } 
 module.exports = repeat;
 console.log("count display ",repeat(operation,10)===10);
+counter=0;
 console.log("Should not pass for negative number", repeat(operation,-1)===false);
+counter=0;
+console.log("Should work for 0 ", repeat(operation,0)===0); 
