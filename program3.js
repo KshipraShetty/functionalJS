ecc6e2c60ba429c1aeb0bcde27d8dc7d0d8dcc94
kshiprashetty <kshiprashetty@gmail.com>
2018-01-16 14:50:16 +0530

    Commited the base case

---
+++ program3.js
@@ -0,0 +1,21 @@
+function getShortMessages(arrayMessages) {
+        let output = arrayMessages.filter(function(obj) {
+                                        if(obj.message.length<50)
+                                                return obj;
+                                                })
+        return output.map( function(element) {
+                                        return element.message;
+                                        })
+
+
+}
+
+
+let arrayObj = [ {message: "hello there!"},
+                 { message: "Whats up" },
+                 { message: "dfghjkfyghouxhusxhusxuosbsu xbsuxbu qwertyuiopasdfghjkl    jkokzijiohxousxhugxugwux"}
+                ]
+
+
+console.log("Testing the base case :", getShortMessages(arrayObj))
+~
