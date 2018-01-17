cb3566e561e40a9beb26bcce8b885df7122685a9
kshiprashetty <kshiprashetty@gmail.com>
2018-01-17 11:27:05 +0530

    Normal case

--- program6.js
+++ program6.js
@@ -15,5 +15,5 @@ const inputWords = [];
 console.log('Base case of empty array ', countWords(inputWords).toString() === {}.toString());
 
 // normal inputWords
-//inputWords = ['Apple', 'Banana', 'Apple'];
-//console.log('Base case of array ', JSON.stringify(countWords(inputWords)) === JSON.stringify({ Apple: 2, Banana: 1 }));
+inputWords = ['Apple', 'Banana', 'Apple'];
+console.log('Base case of array ', JSON.stringify(countWords(inputWords)) === JSON.stringify({ Apple: 2, Banana: 1 }));
